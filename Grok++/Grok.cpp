b555ec409c326cb849d60107cdc22d44465d6357
nick <nick@oppen.net.au>
2023-09-12 16:34:13 +1000

    Initial Commit fixups

--- Grok++/Grok.cpp
+++
@@ -1,8 +0,0 @@
-//#include "Grok.h"
-//
-//const regex Grok::_grokNamedExpression = regex("%\\{(\\w+):(\\w+)(?::\\w+)?\\}");	// all named expressions 
-//const regex Grok::_grokUnnamedExpression = regex("%\\{(\\w+)\\}");					// all unmaned expressions
-//
-//const regex Grok::_grokRegexWithType = regex("(%\\{(\\w+):(\\w+):(\\w+)\\})*?");	// typed expressions
-
-// No longer used
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+# PiholeLogAnalyser++
+An interpreter/loader for pihole log files.
+
+Written in C++ and developed on the pi.
+
 # Grok++
+A C++ implementation of Roman Marusyk's Grok.Net (https://github.com/Marusyk/grok.net). All credit for the design goes to him.
